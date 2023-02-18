644f5050194ede4f6182e0d48a4c0c5cc0dd23f5
Guilherme Suttanni <guilherme.ferreira@mv.com.br>
2023-02-18 15:15:24 -0300

    Adicionado opção de excluir o item na coluna de já comprados

--- index.js
+++ index.js
@@ -1,37 +1,59 @@
-// Selecting DOM elements
+// Select HTML elements
 const newItemForm = document.getElementById("new-item-form");
 const newItemInput = document.getElementById("new-item-input");
 const toBuyList = document.getElementById("to-buy");
 const purchasedList = document.getElementById("purchased");
 
-// Add item to "To Buy" list
+// Function to create a new item and add it to the "To Buy" list
 function addItem(event) {
   event.preventDefault(); // Prevent form from submitting
-  const itemText = newItemInput.value.trim(); // Trim whitespace from input
-  if (itemText !== "") {
-    const item = document.createElement("li");
-    const checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-    checkbox.addEventListener("click", moveItem);
-    item.appendChild(checkbox);
-    const text = document.createTextNode(itemText);
-    item.appendChild(text);
+
+  // Create new item with checkbox and text content
+  const item = document.createElement("li");
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.addEventListener("click", moveItem);
+  const text = document.createTextNode(newItemInput.value);
+  item.appendChild(checkbox);
+  item.appendChild(text);
+
+  // Add new item to "To Buy" list
+  toBuyList.appendChild(item);
+
+  // Clear input field
+  newItemInput.value = "";
+}
+
+// Function to move item to "Already Purchased" list and add delete icon
+function moveItem(event) {
+  const item = event.target.parentNode;
+  if (event.target.checked) {
+    // Move item to "Already Purchased" list
+    purchasedList.appendChild(item);
+    item.classList.add("checked");
+
+    // Add delete icon to item
+    const deleteIcon = document.createElement("span");
+    deleteIcon.className = "delete-icon";
+    deleteIcon.addEventListener("click", deleteItem);
+    item.appendChild(deleteIcon);
+  } else {
+    // Move item back to "To Buy" list
     toBuyList.appendChild(item);
-    newItemInput.value = ""; // Clear input field
+    item.classList.remove("checked");
+
+    // Remove delete icon from item
+    const deleteIcon = item.querySelector(".delete-icon");
+    if (deleteIcon) {
+      item.removeChild(deleteIcon);
+    }
   }
 }
 
-// Move item to "Already Purchased" list
-function moveItem(event) {
+// Function to delete item from "Already Purchased" list
+function deleteItem(event) {
   const item = event.target.parentNode;
-  const checkbox = item.querySelector("input[type=checkbox]");
-  if (checkbox.checked) {
-    purchasedList.appendChild(item);
-    item.classList.add("checked");
-  } else {
-    toBuyList.appendChild(item);
-    item.classList.remove("checked");
-  }
+  purchasedList.removeChild(item);
 }
 
 // Add event listener to form
